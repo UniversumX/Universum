cd204e7674e2cebd596089849d52126ed83e349b
Ajith Senthil <ajithksenthil@gmail.com>
2023-12-28 20:19:54 -0600

    testing 1d CNN and added basic architecture for the transformer blocks based on EEG Former

--- ModelDevelopment/main.py
+++ ModelDevelopment/main.py
@@ -1,6 +1,35 @@
-from model import EEGModel
+from model import EEGModel, CNN1D
+import torch
 config = {
     
 }
 
-m = EEGModel(config)
+# m = EEGModel(config)
+
+def test_cnn1d():
+    batch_size = 10  # Number of samples in a batch
+    sequence_length = 1000  # Number of sampled points per channel
+    n_channels = 8  # Number of EEG channels
+    kernel_size = 3  # Size of the convolution kernel
+    n_1d_cnn_layers = 3  # Number of convolution layers
+    convolution_dimension_length = 64  # Number of filters in convolution layers
+
+    # Create a synthetic EEG data batch
+    synthetic_eeg_data = torch.randn(batch_size, n_channels, sequence_length)
+
+    # Initialize the CNN1D model
+    print("sequence length", sequence_length)
+    print("convolution_dimension_length", convolution_dimension_length)
+    print("kernel_size", kernel_size)
+    print("n_1d_cnn_layers", n_1d_cnn_layers)
+    print("n_channels", n_channels)
+    cnn1d_model = CNN1D(sequence_length, convolution_dimension_length, kernel_size, n_1d_cnn_layers, n_channels)
+
+    # Pass the synthetic data through the model
+    output = cnn1d_model(synthetic_eeg_data)
+    print("output shape", output.shape)
+    # Check the output shape
+    assert output.shape == (batch_size, n_channels, sequence_length), "Output shape mismatch"
+
+# Run the test
+test_cnn1d()
--- ModelDevelopment/model.py
+++ ModelDevelopment/model.py
@@ -1,11 +1,163 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class CNN1D(nn.Module):
     """
-    This was mostly stolen from the EEGFormer implementation I found on github
+    Modified EEGFormer 1D CNN implementation using depth-wise convolution.
+    TODO this may need to be changed for the actual data
     """
+
+    def __init__(
+        self,
+        sequence_length: int,
+        convolution_dimension_length: int,
+        kernel_size: int,
+        n_1d_cnn_layers: int,
+        n_channels=8,
+    ):
+        super(CNN1D, self).__init__()
+        self.n_channels = n_channels  # Number of channels
+        self.sequence_length = sequence_length  # Number of sampled points
+
+        # Ensure at least one layer
+        assert n_1d_cnn_layers >= 1, "Number of 1D CNN layers must be at least 1"
+
+        # Initial depth-wise convolution layer
+        self.initial_conv = nn.Conv1d(n_channels, n_channels * convolution_dimension_length, kernel_size=kernel_size, groups=n_channels, padding=(kernel_size - 1) // 2)
+
+        # Subsequent depth-wise convolution layers
+        self.subsequent_convs = nn.ModuleList([
+            nn.Conv1d(n_channels * convolution_dimension_length, n_channels * convolution_dimension_length, kernel_size=kernel_size, groups=n_channels, padding=(kernel_size - 1) // 2)
+            for _ in range(1, n_1d_cnn_layers)
+        ])
+
+    def forward(self, x):
+        """
+        Expected input shape: (batch_size, n_channels, sequence_length)
+        """
+        # Apply initial depth-wise convolution
+        output = self.initial_conv(x)
+
+        # Apply subsequent depth-wise convolutions
+        for conv in self.subsequent_convs:
+            output = conv(output)
+
+        # Reshape the output to maintain the sequence length
+        batch_size, _, _ = x.shape
+        output = output.view(batch_size, self.n_channels, -1)
+
+        # Ensure output sequence length matches input sequence length
+        output = output[:, :, :self.sequence_length]
+
+        return output
+
+# Example usage
+# model = CNN1D(sequence_length=1000, convolution_dimension_length=64, kernel_size=3, n_1d_cnn_layers=3, n_channels=8)
+# input_data = torch.randn(10, 8, 1000)  # synthetic data
+# output = model(input_data)
+# print(output.shape)  # should be torch.Size([10, 8, 1000])
+
+
+
+
+# TODO make sure this can generalize
+class TransformerBlock(nn.Module):
+    def __init__(self, input_dim, num_heads, ff_dim, dropout=0.1):
+        super(TransformerBlock, self).__init__()
+        self.attention = nn.MultiheadAttention(input_dim, num_heads, dropout=dropout)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(input_dim, ff_dim),
+            nn.ReLU(),
+            nn.Linear(ff_dim, input_dim)
+        )
+        self.layer_norm1 = nn.LayerNorm(input_dim)
+        self.layer_norm2 = nn.LayerNorm(input_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # Self-attention layer
+        attn_output, _ = self.attention(x, x, x)
+        x = x + self.dropout(attn_output)
+        x = self.layer_norm1(x)
+
+        # Feed-forward layer
+        ff_output = self.feed_forward(x)
+        x = x + self.dropout(ff_output)
+        x = self.layer_norm2(x)
+        return x
+
+# TODO implement
+class TemporalTransformer(nn.Module):
+    def __init__(self, input_dim, num_heads, ff_dim, num_layers, dropout=0.1):
+        super(TemporalTransformer, self).__init__()
+        self.layers = nn.ModuleList([TransformerBlock(input_dim, num_heads, ff_dim, dropout) for _ in range(num_layers)])
+
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+# TODO implement
+class SynchronousTransformer(nn.Module):
+    def __init__(self, input_dim, num_heads, ff_dim, num_layers, dropout=0.1):
+        super(SynchronousTransformer, self).__init__()
+        self.layers = nn.ModuleList([TransformerBlock(input_dim, num_heads, ff_dim, dropout) for _ in range(num_layers)])
+
+    def forward(self, x):
+        # x should be organized to emphasize synchronous aspects
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+
+class RegionalTransformer(nn.Module):
+    def __init__(self, input_dim, num_heads, ff_dim, num_layers, dropout=0.1):
+        super(RegionalTransformer, self).__init__()
+        self.layers = nn.ModuleList([TransformerBlock(input_dim, num_heads, ff_dim, dropout) for _ in range(num_layers)])
+
+    def forward(self, x):
+        # x should be organized to emphasize regional aspects
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+
+
+
+class EEGformerEncoder(nn.Module):
+    def __init__(self, input_dim, num_heads, ff_dim, num_layers, dropout=0.1):
+        super(EEGformerEncoder, self).__init__()
+        self.temporal_transformer = TemporalTransformer(input_dim, num_heads, ff_dim, num_layers, dropout)
+        self.synchronous_transformer = SynchronousTransformer(input_dim, num_heads, ff_dim, num_layers, dropout)
+        self.regional_transformer = RegionalTransformer(input_dim, num_heads, ff_dim, num_layers, dropout)
+
+    def forward(self, x):
+        x = self.temporal_transformer(x)
+        x = self.synchronous_transformer(x)
+        x = self.regional_transformer(x)
+        return x
+
+
+
+class EEGModel(torch.nn.Module):
+    def __init__(
+        self,
+    ):
+        super(EEGModel, self).__init__()
+
+    def forward(self, x):
+        pass
+
+
+"""
+
+class CNN1D(nn.Module):
+
+    # This was mostly stolen from the EEGFormer implementation I found on github
+
 
     def __init__(
         self,
@@ -33,10 +185,10 @@ class CNN1D(nn.Module):
             )
 
     def forward(self, x):
-        """
-        Expected input shape:
-        (batch_size, n_channels, sequence_lengths)
-        """
+
+        #Expected input shape:
+        #(batch_size, n_channels, sequence_lengths)
+ 
         outputs = []
         # The idea of this loop is that we use the same 1d conv layer on every single channel to extract artifacts from it,
         for i in range(self.n_channels):
@@ -51,13 +203,31 @@ class CNN1D(nn.Module):
         #! If the output_tensor is not at the seuqnece_length length, then pad it with 0's?'
         output_tensor = output_tensor[:, :, : self.sequence_length]
         return output_tensor
+"""
 
 
-class EEGModel(torch.nn.Module):
-    def __init__(
-        self,
-    ):
-        super(EEGModel, self).__init__()
 
-    def forward(self, x):
-        pass
+"""
+the EEGformer model specialize in different aspects of the neural signal (temporal, regional, and synchronous) is significantly determined during the dataset preparation and how the data is fed into these transformers. The architecture of the transformers themselves doesn't inherently distinguish between these characteristics; it's the structure and preprocessing of the input data that dictates what each transformer focuses on.
+
+Here's how this specialization typically works in the context of EEG data:
+
+Temporal Transformer
+Focus: Processes the temporal aspects of EEG data.
+Data Preparation: The input data should emphasize the temporal dynamics. This means organizing the EEG data so that the sequence input to the transformer represents different time points. The transformer will then learn patterns across these temporal sequences.
+Synchronous Transformer
+Focus: Deals with synchronous patterns of brain activity across different channels.
+Data Preparation: The input data should be structured to highlight the synchronous activity across different EEG channels at the same time point. This might involve reorganizing the data so that for each time point, the input features represent the simultaneous readings from different EEG channels.
+Regional Transformer
+Focus: Handles different brain regions.
+Data Preparation: The input data should be prepared in a way that emphasizes spatial (regional) relationships. This could involve structuring the data such that the input to the transformer at each time step represents data from different brain regions.
+Implementation Consideration
+Dataset Structure: Careful structuring of the input data is crucial. This involves reshaping and organizing the EEG data appropriately before it's fed into each transformer.
+Feature Extraction: Initial layers (like the 1DCNN you might use) are responsible for extracting relevant features from raw EEG data, which the transformers then process. The way these features are extracted and organized can greatly influence the focus of each transformer.
+Sequential Processing: The EEGformer model processes the data sequentially through these transformers. The output of one becomes the input to the next, adding layers of contextual understanding (temporal, synchronous, regional) at each stage.
+Example
+Consider an EEG dataset with readings from multiple channels over time. For the Synchronous Transformer, you'd organize the data so that for each time step, the feature vector represents concurrent readings from all channels. For the Regional Transformer, you'd organize the data to focus on spatial patterns, perhaps grouping channels according to their location on the scalp.
+
+In essence, while the transformer architecture is capable of capturing complex relationships in the data, it's the way the data is presented to each transformer that directs its focus towards temporal, synchronous, or regional aspects of the EEG signal.
+
+"""
