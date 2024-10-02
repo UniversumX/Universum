4e3d7d4c63b85e70d849a2f7039276f2ee46fe7a
MattHandzel <matthewwalkosz@gmail.com>
2024-10-02 17:16:34 -0500

    Adding data collected stuff

--- DataCollection/actions.py
+++ DataCollection/actions.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Action:
     action_value: int
@@ -7,49 +8,60 @@ class Action:
     audio: str
     image: str
 
+
 actions = {
-    "reset": Action(
-        action_value=0,
-        text="",
-        audio=None,
-        image=None
-    ),
+    "reset": Action(action_value=0, text="", audio=None, image=None),
     "left_elbow_flex": Action(
         action_value=1,
         text="Please flex your left elbow so your arm raises to shoulder level",
         audio="path/to/audio",
-        image="path/to/image"
-        ),
+        image="path/to/image",
+    ),
     "left_elbow_relax": Action(
         action_value=2,
         text="Please relax your left elbow back to original state",
         audio="path/to/audio",
-        image="path/to/image"
-        ),
+        image="path/to/image",
+    ),
     "right_elbow_flex": Action(
         action_value=3,
         text="Please flex your right elbow so your arm raises to shoulder level",
         audio="path/to/audio",
-        image="path/to/image"
-        ),
+        image="path/to/image",
+    ),
     "right_elbow_relax": Action(
         action_value=4,
         text="Please relax your right elbow back to original state",
         audio="path/to/audio",
-        image="path/to/image"
-        ),
+        image="path/to/image",
+    ),
     "end_collection": Action(
-        action_value=5,
-        text="Data collection ended",
-        audio=None,
-        image=None
-    )
+        action_value=5, text="Data collection ended", audio=None, image=None
+    ),
 }
 
+
 procedures = [
-    (5, "left_elbow_flex"),
-    (10, "left_elbow_relax"),
-    (15, "right_elbow_flex"),
-    (20, "right_elbow_relax"),
-    (25, "end_collection")
+    ## Repetition 1
+    (3, "left_elbow_flex"),
+    (6, "left_elbow_relax"),
+    (9, "right_elbow_flex"),
+    (12, "right_elbow_relax"),
+    ## Repetition 2
+    (15 + 2, "left_elbow_flex"),
+    (18 + 2, "left_elbow_relax"),
+    (21 + 2, "right_elbow_flex"),
+    (24 + 2, "right_elbow_relax"),
+    ## Repetition 3
+    (27 + 4, "left_elbow_flex"),
+    (30 + 4, "left_elbow_relax"),
+    (33 + 4, "right_elbow_flex"),
+    (36 + 4, "right_elbow_relax"),
+    ## Repetition 4
+    (39 + 6, "left_elbow_flex"),
+    (42 + 6, "left_elbow_relax"),
+    (45 + 6, "right_elbow_flex"),
+    (48 + 6, "right_elbow_relax"),
+    ## End :)
+    (51 + 6, "end_collection"),
 ]
